6be7337954744b55f0495f1b5f92d50d1d48bc6f
simeon babatunde <simeon@114.213.21.198.tigernet.wifi.dyn.clemson.edu>
2019-11-19 19:47:45 -0500

    Updated logistic regression

---
+++ OnevsAll.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+import numpy as np
+from utils import *
+import matplotlib.pyplot as plt
+
+
+
+class LogisticRegressionOVR(object):
+    def __init__(self, eta=0.1, n_iter=50):
+        self.eta = eta
+        self.n_iter = n_iter
+
+    def _sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+
+    def _predict_one(self, x):
+        return max((x.dot(w), c) for w, c in self.w)[1]
+
+    def predict(self, X):
+        return [self._predict_one(i) for i in np.insert(X, 0, 1, axis=1)]
+
+    def score(self, X, y):
+        return sum(X == y) / len(y)
+
+    def fit(self, X, y):
+        X = np.insert(X, 0, 1, axis=1)
+        self.w = []
+        m = X.shape[0]
+
+        for i in np.unique(y):
+            y_copy = np.where(y == i, 1, 0)
+            w = np.ones(X.shape[1])
+
+            for _ in range(self.n_iter):
+                output = X.dot(w)
+                errors = y_copy - self._sigmoid(output)
+                w += self.eta / m * errors.dot(X)
+            self.w.append((w, i))
+        return self
+
+
+if __name__ == '__main__':
+    # Initial weights
+    np.random.seed(100)
+    wts = np.random.rand(1,13)[0]
+    alpha = 0.05
+    epocs = 800
+    error_epocs = np.zeros([epocs,2])
+
+    dataset = read_file("activity_recognition_dataset.csv")
+    train,test = data_preprocess(dataset, 0.8)
+
+    multiclass = LogisticRegressionOVR(eta=alpha, n_iter=epocs).fit(train[:, :-1], train[:, -1])
+    prediction = multiclass.predict(test[:, :-1])
+
+    print(compute_score(prediction, test[:, -1]))
+    print_accuracy_stats(prediction, test[:, -1])
--- main.py
+++ main.py
@@ -1,11 +1,50 @@
 #!/usr/bin/env python
 import numpy as np
 from utils import *
+import matplotlib.pyplot as plt
 
 
 if __name__ == '__main__':
+    # Initial weights
+    np.random.seed(100)
+    wts = np.random.rand(1,13)[0]
+    alpha = 0.05
+    epocs = 1000
+    error_epocs = np.zeros([epocs,2])
+    weights_array = np.zeros([3,13])            # This saves weights
+
     dataset = read_file("activity_recognition_dataset.csv")
     train,test = data_preprocess(dataset, 0.8)
 
-    print(train.shape)
-    print(test.shape)
+    # Loop through the 3 classes by comparing one against others
+    # Keep an array of weights for those 3 runs
+    # for i in range(3):
+    #     # Set label to 1 for corresponding i and others to zero
+    #     label = (train[:, -1] == i).astype(int)
+    #     train[:, -1] = label
+    #     # Compute Error and save in array
+    #     error_j = j_error(wts, train)
+    #     error_epocs[0][0] = 0
+    #     error_epocs[0][1] = error_j
+    #
+    #     # Train the algorithm for some epocs
+    #     for epoc in range(epocs - 1):
+    #         wts = gradient_descent(wts, alpha, train)
+    #         error_j = j_error(wts, train)
+    #         error_epocs[epoc+1][0] = epoc + 1
+    #         error_epocs[epoc+1][1] = error_j
+    #
+    #     for m in range(13):
+    #         weights_array[i,m] = wts[m]
+    #     plot_error(error_epocs, str(i+1))
+    #
+    #
+    # np.savetxt("weigths.csv", weights_array, delimiter=",")
+    #
+    weights_arr = np.loadtxt(fname = "weigths.csv", delimiter=',')
+
+
+    predictions = predict(weights_array, test[:, :-1])
+
+    correct = compute_correction(predictions, test[:, -1])
+    print(correct, float(correct)/len(predictions))
